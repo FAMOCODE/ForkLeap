c7b15bf7b7135c7faa0f58e4402f425d2d9b4cb7
fameless0_o <cjcrecordz@gmail.com>
2025-12-08 08:42:16 -0600

    init

--- assets/scripts/player/player_controller.gd
+++ assets/scripts/player/player_controller.gd
@@ -1,6 +1,16 @@
 class_name PlayerController extends RigidBody3D
-
+var is_on_floor = false
 @export var debug : bool = false
 @export_category("References")
 @export var camera3d : CameraController
+var friction = 1.0
+var velocity=Vector3()
+
 var gravity = ProjectSettings.get_setting("physics/jolt_physics_3d/joints/world_node")
+
+var speed : float = 0.0 
+var stop_speed : float= 0.1
+
+func _physics_process(_delta):
+	if friction >= 0: friction = 0
+	is_on_floor = false
