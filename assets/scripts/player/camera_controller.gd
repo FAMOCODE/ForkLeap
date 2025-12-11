b382a71b3de7e3a651475b0fa0195a4cd81b43cb
fameless0_o <cjcrecordz@gmail.com>
2025-12-10 21:05:06 -0600

    init

--- assets/scripts/player/camera_controller.gd
+++ assets/scripts/player/camera_controller.gd
@@ -6,8 +6,7 @@ class_name CameraController extends Node3D
 @export var component_mouse_capture : MouseCaptureComponent
 @export_category("Camera Settings")
 @export_group("Camera Tilt")
-@export_range(-90,-60) var tilt_lower_limit: int = -90
-@export_range(60,90) var tilt_upper_limit : int = 90
+
 
 
 
@@ -19,7 +18,7 @@ func _process(_delta: float) -> void:
 func update_camera_rotation(input: Vector2) -> void:
 	_rotation.x += input.y
 	_rotation.y += input.x
-	_rotation.x = clamp(_rotation.x, deg_to_rad(tilt_lower_limit), deg_to_rad(tilt_upper_limit))
+	
 		
 	_rotation.z = 0.0
 	rotation = Vector3(_rotation.x, _rotation.y, 0.0)
